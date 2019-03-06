2eaee77bccd93905db44dd8edeaed65507afafa5
abdallah nasser <abdu.abdallah22@gmail.com>
2019-03-06 15:30:59 +0200

    fix service worker

--- js/sw_registration.js
+++ js/sw_registration.js
@@ -1,7 +1,13 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js').then(function() {
-    console.log('Registration worked!');
-  }).catch(function() {
-    console.log('Registration failed!');
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
   });
+} else {
+	console.log('Service Worker is not supported in this browser.');
 }
